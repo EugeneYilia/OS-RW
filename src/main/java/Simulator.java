cc2b667d7fedee005eb38091c54dccfcd225418e
EugeneYilia <liuyichen@eugeneliu.xyz>
2018-06-21 05:50:24 +0800

    add some situation

--- src/main/java/Simulator.java
+++ src/main/java/Simulator.java
@@ -9,11 +9,44 @@ public class Simulator {
         Thread writerThread2 = new Thread(new Writer());
         Thread writerThread3 = new Thread(new Writer());
 
+        //situation 1.
+        /*
         readerThread1.start();
         readerThread2.start();
         readerThread3.start();
         writerThread1.start();
         writerThread2.start();
         writerThread3.start();
+        */
+
+        //situation 2.
+        /*
+        readerThread1.start();
+        writerThread1.start();
+        readerThread2.start();
+        writerThread2.start();
+        readerThread3.start();
+        writerThread3.start();
+         */
+
+        //situation 3.
+        /*
+        writerThread1.start();
+        readerThread1.start();
+        writerThread2.start();
+        readerThread2.start();
+        writerThread3.start();
+        readerThread3.start();
+         */
+
+        //situation 4.
+        /*
+        writerThread1.start();
+        writerThread2.start();
+        writerThread3.start();
+        readerThread1.start();
+        readerThread2.start();
+        readerThread3.start();
+         */
     }
 }
